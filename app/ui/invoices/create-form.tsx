15b5cbc2f4a5fc988134d21ff022a6aa71a7afd1
Johnattan Silva Ferreira <johnattan.silva.ferreira@gmail.com>
2024-11-24 18:21:45 -0300

    feat: add validation error messages for missing form fields in update invoices page
    - Display error messages for missing customer selection, amount input, and invoice status.

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -70,12 +70,14 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
-            {state.errors?.amount &&
-              state.errors?.amount.map((error: string) => (
-                <p className="mt-2 text-sm text-red-500" key={error}>
-                  {error}
-                </p>
+            <div id="customer-error" aria-live="polite" aria-atomic="true">
+              {state.errors?.amount &&
+                state.errors?.amount.map((error: string) => (
+                  <p className="mt-2 text-sm text-red-500" key={error}>
+                    {error}
+                  </p>
               ))}
+            </div>
           </div>
         </div>
 
@@ -118,12 +120,14 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               </div>
             </div>
           </div>
-          {state.errors?.status &&
-              state.errors?.status.map((error: string) => (
-                <p className="mt-2 text-sm text-red-500" key={error}>
-                  {error}
-                </p>
-          ))}
+          <div id="customer-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.status &&
+                state.errors?.status.map((error: string) => (
+                  <p className="mt-2 text-sm text-red-500" key={error}>
+                    {error}
+                  </p>
+            ))}
+          </div>
           <p className="mt-2 text-sm text-red-500">
             {state.message}
           </p>
